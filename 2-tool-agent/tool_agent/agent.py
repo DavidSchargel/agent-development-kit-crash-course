5e6cb3a5a8b461434d8ac5b37403f6f3f686ecb5
David Schargel <david@schargel.com>
2025-06-18 21:55:16 -0700

    feat: try function get_current_time tool and set aside google_search functionality

--- 2-tool-agent/tool_agent/agent.py
+++ 2-tool-agent/tool_agent/agent.py
@@ -1,13 +1,22 @@
-from google.adk.agents import Agent
-from google.adk.tools import google_search
+"""
+Tool Agent.
 
-# def get_current_time() -> dict:
-#     """
-#     Get the current time in the format YYYY-MM-DD HH:MM:SS
-#     """
-#     return {
-#         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-#     }
+This agent uses tools to perform tasks.
+"""
+
+from datetime import datetime
+
+from google.adk.agents import Agent
+
+# from google.adk.tools import google_search
+
+
+def get_current_time() -> dict:
+    """Get the current time in the format YYYY-MM-DD HH:MM:SS."""
+    return {
+        "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),  # noqa: DTZ005
+    }
+
 
 root_agent = Agent(
     name="tool_agent",
@@ -15,9 +24,9 @@ root_agent = Agent(
     description="Tool agent",
     instruction="""
     You are a helpful assistant that can use the following tools:
-    - google_search
+    - get_current_time
     """,
-    tools=[google_search],
-    # tools=[get_current_time],
+    # tools=[google_search],
+    tools=[get_current_time],
     # tools=[google_search, get_current_time], # <--- Doesn't work
 )
